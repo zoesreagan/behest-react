46ec022fdbe0a7bef9965d1b259e7c9b25a16487
Zoe <zoereagan@gmail.com>
2018-07-04 18:19:55 -0500

    nav issues

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,72 +1,44 @@
 import React, { Component } from 'react';
-import { Button, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
+class Navbar extends Component {
+  constructor(){
+    super();
     this.state = {
-      collapsed: true
-    };
+      logout: false,
+    }
   }
 
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-  render() {
+
+  render(){
     return (
-      <div>
-        <Navbar color="faded" light>
-          <NavbarBrand href="/" className="mr-auto">Behest</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav navbar>
-              <NavItem>
-                <Button color="primary" onClick={this.props.navigateToIndex}>Home</Button>
-              </NavItem>
-              <NavItem>
-                <Button color="info" onClick={this.props.renderAddNewUserForm}>Create New Form</Button>
-              </NavItem>
-              <NavItem>
-                <Button color="secondary" onClick={this.props.logout}>Logout</Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
+        <div className="container">
+          <div className="row">
+
+
+              <div className="four columns">
+                <button className="button button-primary" onClick={this.props.navigateToIndex}>Home</button>
+              </div>
+
+
+              {/* <div className="three columns">
+              <button className="button button" onClick={this.props.openLearnMorePage}>Learn More</button>
+            </div> */}
+
+
+            <div className="four columns">
+                  <button className="button button" onClick={this.props.renderAddNewUserForm}>Create New Form</button>
+            </div>
+
+
+            <div className="four columns">
+              <button className="button button" onClick={this.props.logout}>Logout</button>
+            </div>
+
+          </div>
+        </div>
+    )
   }
 }
-  // render(){
-  //   return (
-  //       <div className="container">
-  //         <div className="row">
-  //
-  //             <div className="four columns">
-  //               <button className="button button-primary" onClick={this.props.navigateToIndex}>Home</button>
-  //             </div>
-  //
-  //
-  //             {/* <div className="three columns">
-  //             <button className="button button" onClick={this.props.openLearnMorePage}>Learn More</button>
-  //           </div> */}
-  //
-  //
-  //           <div className="four columns">
-  //                 <button className="button button" onClick={this.props.renderAddNewUserForm}>Create New Form</button>
-  //           </div>
-  //
-  //
-  //           <div className="four columns">
-  //             <button className="button button" onClick={this.props.logout}>Logout</button>
-  //           </div>
-  //
-  //         </div>
-  //       </div>
-  //   )
-  // }
+
 
 export default Navbar;
